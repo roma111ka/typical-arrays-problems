4091dbc69edc4ff9c34483c5aa8182a6a820505b
Роман Лодяты Александрович <68326671+roma111ka@users.noreply.github.com>
2021-08-15 18:40:15 +0300

    feat: tip arr problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length ===0) return 0;
+  let minNumber = array[0];
+  for(let i =0;i<array.length;i++){
+    if (array[i] < minNumber) minNumber = array[i]; 
+  }
+  return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length ===0) return 0;
+  let maxNumber = array[0];
+  for(let i =0;i<array.length;i++){
+    if (array[i] > maxNumber) maxNumber = array[i]; 
+  }
+  return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length ===0) return 0;
+  let sum = 0;
+  for(let i =0;i<array.length;i++){
+    sum += array[i];
+  }
+  return sum/array.length;
 }
